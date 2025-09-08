43bddd706ef045c5a908d7ca0ad5d69a982d6f1f
tranongtan-max <tranongtan@gmail.com>
2025-09-08 14:51:22 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+{
+  "name": "kaka-server",
+  "type": "module",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.19.2",
+    "node-fetch": "^3.3.2",
+    "socket.io": "^4.7.5"
+  }
+}
